73042f0d54c1c39a819c5ca1708e32552edf36c0
ahmedayman4a <ahmedayman4a77@gmail.com>
2025-03-15 23:05:28 +0200

    change evaluation interval from 5 to 1

--- exps/example/custom/mot.py
+++ exps/example/custom/mot.py
@@ -15,7 +15,7 @@ class Exp(MyExp):
         self.random_size = (20, 36)
         self.max_epoch = 80
         self.print_interval = 20
-        self.eval_interval = 5
+        self.eval_interval = 1
         self.test_conf = 0.001
         self.nmsthre = 0.7
         self.no_aug_epochs = 10
