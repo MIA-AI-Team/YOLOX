3f993c76ead1034724c539bd53a80515c6b583f9
ahmedayman4a <ahmedayman4a77@gmail.com>
2025-03-15 20:34:06 +0200

    change lr

--- exps/example/custom/mot.py
+++ exps/example/custom/mot.py
@@ -19,5 +19,5 @@ class Exp(MyExp):
         self.test_conf = 0.001
         self.nmsthre = 0.7
         self.no_aug_epochs = 10
-        self.basic_lr_per_img = 0.001 / 8.0
+        self.basic_lr_per_img = 0.001 / 4.0
         self.warmup_epochs = 1
