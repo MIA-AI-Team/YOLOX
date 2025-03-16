0923eca57417b66fdf5d01e064ac2e8de00b8c43
ahmedayman4a <ahmedayman4a77@gmail.com>
2025-03-16 16:35:13 +0200

    add mot 640x640

---
+++ exps/example/custom/mot_640.py
@@ -0,0 +1,23 @@
+from yolox.exp import Exp as MyExp
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.num_classes = 1
+        self.depth = 1.33
+        self.width = 1.25
+        self.data_dir = "datasets/MOT20"
+        self.train_ann = "train.json"
+        self.val_ann = "val.json"
+        self.input_size = (640, 640)
+        self.test_size = (640, 640)
+        #self.test_size = (736, 1920)
+        self.random_size = (20, 36)
+        self.max_epoch = 80
+        self.print_interval = 20
+        self.eval_interval = 1
+        self.test_conf = 0.001
+        self.nmsthre = 0.7
+        self.no_aug_epochs = 10
+        self.basic_lr_per_img = 0.001 / 4.0
+        self.warmup_epochs = 1
