19bb67a40b37ccd16c8b75d786b2b5af28b3ac6d
Ahmed Ayman <ahmedayman4a77@gmail.com>
2025-03-18 16:44:20 +0200

    change no_aug_epochs from 10 to 0

--- exps/example/custom/mot_640_m.py
+++ exps/example/custom/mot_640_m.py
@@ -18,7 +18,7 @@ class Exp(MyExp):
         self.eval_interval = 1
         self.test_conf = 0.001
         self.nmsthre = 0.7
-        self.no_aug_epochs = 10
+        self.no_aug_epochs = 0
         self.basic_lr_per_img = 0.001 / 16.0
         self.warmup_epochs = 1
         self.save_history_ckpt = False
